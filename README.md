98cc32336890f912119fb8a4d8ddfd13f770a4e8
hasan ahani <hasanahani.ir@gmail.com>
2021-04-08 02:02:26 +0430

    start

---
+++ README.md
@@ -0,0 +1,74 @@
+# nice-toast-js
+#### Made with create-jquery-plugin
+
+[![npm version](https://badge.fury.io/js/nice-toast-js.svg)](https://www.npmjs.com/package/nice-toast-js)
+
+Requirements
+-----
+  + [jQuery](http://jquery.com/)
+
+
+Installation
+-----
+
+### [NPM](https://www.npmjs.com/package/nice-toast-js)
+```bash
+npm install nice-toast-js
+```
+
+### [Yarn](https://yarn.pm/nice-toast-js)
+```bash
+yarn add nice-toast-js
+```
+
+### [CDN - jsDelivr](https://www.jsdelivr.com/package/npm/nice-toast-js)
+```html
+<!-- CSS -->
+<link href="https://cdn.jsdelivr.net/npm/nice-toast-js/dist/css/nice-toast-js.min.css" rel="stylesheet" type="text/css" />
+
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/nice-toast-js/dist/js/nice-toast-js.min.js" type="text/javascript"></script>
+```    
+
+### [CDN - UNPKG](https://unpkg.com/browse/nice-toast-js/)
+```html
+<!-- CSS -->
+<link href="https://unpkg.com/nice-toast-js/dist/css/nice-toast-js.min.css" rel="stylesheet" type="text/css" />
+
+<!-- JavaScript -->
+<script src="https://unpkg.com/nice-toast-js/dist/js/nice-toast-js.min.js" type="text/javascript"></script>
+```
+
+Usage
+-----
+
+Include CSS
+```html
+<link href="https://cdn.jsdelivr.net/npm/nice-toast-js/dist/css/nice-toast-js.min.css" rel="stylesheet" type="text/css" />
+```
+
+HTML
+```html
+<div id="element"></div>
+```
+
+Include jQuery
+```html
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+```
+
+Include Plugin JS
+```html
+<script src="https://cdn.jsdelivr.net/npm/nice-toast-js/dist/js/nice-toast-js.min.js" type="text/javascript"></script>
+```
+Initialize the Plugin
+```js
+$('#element').niceToastJs();
+```
+
+License
+----
+MIT
+
+
+Created with :green_heart: [create-jquery-plugin](https://www.npmjs.com/package/create-jquery-plugin)
---
+++ dist/js/nice-toast-js.js
@@ -0,0 +1,166 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+/*!
+ * nice-toast-js 1.0.0
+ * Made with create-jquery-plugin
+ *
+ * Created by hasan ahani <hasanahani.ir@gmail.com>
+ *
+ * @license MIT
+ */
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if ((typeof module === "undefined" ? "undefined" : _typeof(module)) === 'object' && module.exports) {
+    // Node/CommonJS
+    module.exports = function (root, jQuery) {
+      if (jQuery === undefined) {
+        // require('jQuery') returns a factory that requires window to
+        // build a jQuery instance, we normalize how we use modules
+        // that require this pattern but the window provided is a noop
+        // if it's defined (how jquery works)
+        if (typeof window !== 'undefined') {
+          jQuery = require('jquery');
+        } else {
+          jQuery = require('jquery')(root);
+        }
+      }
+
+      factory(jQuery);
+      return jQuery;
+    };
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+})(function ($) {
+  "use strict";
+
+  $('body').append($('<div />').append([$('<div />').append($('<div />').addClass('nice-toast-wrapper top-left')), $('<div />').append($('<div />').addClass('nice-toast-wrapper top-center')), $('<div />').append($('<div />').addClass('nice-toast-wrapper top-right')), $('<div />').append($('<div />').addClass('nice-toast-wrapper bottom-left')), $('<div />').append($('<div />').addClass('nice-toast-wrapper bottom-center')), $('<div />').append($('<div />').addClass('nice-toast-wrapper bottom-right'))])); // Default Options
+
+  var defaults = {
+    position: "top-center",
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+  };
+  var icon_check = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-check-circle fa-w-16 nice-toast-icon"><path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path></svg>';
+  var icon_info = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info-circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-info-circle fa-w-16 nice-toast-icon"><path fill="currentColor" d="M256 8C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm0 110c23.196 0 42 18.804 42 42s-18.804 42-42 42-42-18.804-42-42 18.804-42 42-42zm56 254c0 6.627-5.373 12-12 12h-88c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h12v-64h-12c-6.627 0-12-5.373-12-12v-24c0-6.627 5.373-12 12-12h64c6.627 0 12 5.373 12 12v100h12c6.627 0 12 5.373 12 12v24z"></path></svg>';
+  var icon_danger = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="exclamation-triangle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-exclamation-triangle fa-w-18 nice-toast-icon"><path fill="currentColor" d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"></path></svg>';
+
+  var NiceToastJs = /*#__PURE__*/function () {
+    function NiceToastJs(message, options) {
+      var type = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'default';
+
+      _classCallCheck(this, NiceToastJs);
+
+      // Merge user settings with default
+      this.options = $.extend(true, {}, defaults, options); // Main container element
+
+      this.message = message;
+      this.type = type;
+
+      this._init()._render();
+
+      this._event();
+    }
+
+    _createClass(NiceToastJs, [{
+      key: "_event",
+      value: function _event() {
+        this.toast.bind('mousedown', function (e) {
+          var moved;
+          $(this).bind('mousemove', function (e) {
+            moved = e.pageX - this.offsetLeft;
+            $(this).fadeTo('fast', 1);
+            console.log(moved);
+            console.log(this.offsetLeft);
+          });
+          $(this).bind('mouseup', function () {
+            $(this).unbind('mousemove');
+          });
+        });
+      } // Initial Method
+
+    }, {
+      key: "_init",
+      value: function _init() {
+        var toast,
+            icon,
+            base = 'nice-toast';
+
+        switch (this.type) {
+          case "default":
+          case "info":
+          case "warning":
+            icon = icon_info;
+            break;
+
+          case "error":
+            icon = icon_danger;
+            break;
+
+          case "success":
+            icon = icon_check;
+            break;
+        }
+
+        toast = $('<div />').addClass(base + ' ' + base + '-' + this.type).append([icon, $('<div />').addClass(base + '-content').attr('role', 'alert').text(this.message), $('<button />').addClass(base + '-close').attr('aria-label', 'close').text(' × ')]);
+        this.toast = toast;
+        return this;
+      }
+    }, {
+      key: "_render",
+      value: function _render() {
+        var target = $('.nice-toast-wrapper.' + this.options.position);
+
+        if (target.length) {
+          target.append(this.toast);
+        }
+      }
+    }]);
+
+    return NiceToastJs;
+  }();
+
+  $.niceToast = function (message, options) {
+    if (options === undefined || _typeof(options) === 'object') {
+      return new NiceToastJs(message, options);
+    } else if (typeof options === 'string' && options[0] !== '_' && options !== 'init') {
+      return new NiceToastJs(message, null);
+    }
+  };
+
+  $.niceToast.success = function (message, options) {
+    return $.niceToast(message, options, 'success');
+  };
+
+  $.niceToast.info = function (message, options) {
+    return $.niceToast(message, options, 'info');
+  };
+
+  $.niceToast.warning = function (message, options) {
+    return $.niceToast(message, options, 'warning');
+  };
+
+  $.niceToast.error = function (message, options) {
+    return $.niceToast(message, options, 'error');
+  };
+});
